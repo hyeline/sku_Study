6aac0db1248c1bea5fea85350be088947e054a89
ChoHyeLin <signn123@naver.com>
2022-12-29 17:18:53 +0900

    first web app(/hello)

--- simpleFlask.py
+++ simpleFlask.py
@@ -1,6 +1,15 @@
-a =10
-b=20
-c=a+b
-print(c)
-
-print('hello Flask')
+#필요한 모듈을 단계적으로 import
+from flask import Flask
+ 
+app = Flask(__name__)
+ 
+# 테스트 기능
+@app.route('/hello')
+def hello_Flask():
+    return 'Hello, Flask'
+ 
+if __name__ == '__main__':
+    app.run(
+    host="0.0.0.0",
+    port=7777,
+    debug=True)
